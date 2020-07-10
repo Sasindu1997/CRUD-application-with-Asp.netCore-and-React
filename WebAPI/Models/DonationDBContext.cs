141cc5cde81abf202354cacc692054a4ff394a86
Sasindu1997 <sasinduusgalhewa@gmail.com>
2020-07-10 23:18:25 +0530

    creating db tables in DCandidate model

--- WebAPI/Models/DonationDBContext.cs
+++ WebAPI/Models/DonationDBContext.cs
@@ -7,5 +7,10 @@ namespace WebAPI.Models
 {
     public class DonationDBContext
     {
+        //type of db,
+        //connection details of the db, 
+        //tables in db will be passed in DBContext
+
+
     }
 }
